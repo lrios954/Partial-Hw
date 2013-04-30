257e9149f028ed49ddb7aece008cc1d1731d45af
Leonardo Rios <leo.re4y5@gmail.com>
2013-04-30 13:41:32 -0500

    Pequeño error

--- graficador.py
+++ graficador.py
@@ -4,28 +4,15 @@ import numpy
 
 time_interval = 3.0/10.0
 n_points = 1000;
-	
-for i in range(20):
 
-	if(i < 11):
-		data = numpy.loadtxt(open(str(i)+'.dat', 'r'))
-		u = data[:, 0]
-		x = linspace(0.0, 1.0, n_points)
-		
-		pylab.plot(x, u)
-		pylab.xlabel('x')
-		pylab.ylabel('u(x)')
-		pylab.title('x-u_1')
-		pylab.savefig('x-u_1.png')
+data = numpy.loadtxt(open("inicial1.dat", "r"))
+u = data[:, 1]
+x = data[:, 0]
 	
-	else:
-		data = numpy.loadtxt(open(str(i)+'.dat', 'r'))
-		u = data[:, 0]
-		x = linspace(-5.0, 5.0, n_points)
-		
-		pylab.plot(x, u)
-		pylab.xlabel('x')
-		pylab.ylabel('u(x)')
-		pylab.title('x-u_2')
-		pylab.savefig('x-u_2.png')
-
+pylab.plot(x, u)
+pylab.xlabel('x')
+pylab.ylabel('u(x)')
+pylab.title('x-u_1')
+pylab.savefig('x-u_1.png')
+	
+	
